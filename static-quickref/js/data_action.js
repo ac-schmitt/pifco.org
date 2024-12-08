588a10b9220a34c14c597409bd40c4579e664035
Alexander Schmitt <alexander.schmitt@posteo.de>
2024-12-08 19:00:10 +0100

    feat: update actions to 2024 rules

--- static-quickref/js/data_action.js
+++ static-quickref/js/data_action.js
@@ -15,12 +15,13 @@ data_action = [
             "Some conditions give disadvantage on the attack: attacks against targets with the invisible condition; attacks against targets with the prone condition from more than 5 ft.; attacks while you have the blinded, frightened, poisoned, prone or restrained condition."
         ],
         bullets_de: [
-            "Muss aktualisiert werden:",
-            "Bestimmte Merkmale, wie zum Beispiel der <i>Zusätzliche Angriff</i> des Kämpfers, erlauben mehr als einen Angriff mit dieser Aktion auszuführen. Jeder dieser Angriffe ist ein eigener Angriffswurf und kann sich gegen eine andere Kreatur richten. Du kannst dich zwischen den Angriffen bewegen.",
-			"Wenn du mit einer leichten Nahkampfwaffe angreifst, kannst du eine Bonusaktion verwenden, um mit einer zweiten leichten Nahkampfwaffe in deiner anderen Hand anzugreifen (siehe die Bonusaktion <i>Angriff mit Zweitwaffe</i>).",
-            "Du kannst einen deiner Nahkampfangriffe durch die <i>Ringen</i>- oder <i>Stoßen</i>-Aktion ersetzen.",
-            "Manche Zustände gewähren Vorteil auf einen Angriff: Angriffe gegen blinde, gelähmte, versteinerte, festgesetzte, betäubte oder bewusstlose Ziele; Nahkampfangriffe gegen liegende Ziele; Angriffe durch unsichtbare oder versteckte Angreifer.",
-            "Manche Zustände gewähren Nachteil auf einen Angriff: Angriffe gegen unsichtbare oder versteckte Ziele; Fernkampfangriffe gegen liegende Ziele; Angriffe durch blinde, verängstigte, vergiftete oder festgesetzte Angreifer." 
+            "Wenn du diese Aktion ausführst, kannst du einen Angriffswurf mit einer Waffe oder einem unbewaffneten Schlag ausführen.",
+            "Du kannst eine Waffe entweder ausrüsten oder ablegen, wenn du im Rahmen dieser Aktion einen Angriff ausführst.",
+			"Bestimmte Eigenschaften, wie z.B. die <i>Extra Attack</i> Eigenschaft des Kämpfers, erlauben es dir, mehr als einen Angriff mit dieser Aktion durchzuführen. Jeder dieser Angriffe ist ein separater Wurf und kann auf verschiedene Kreaturen abzielen. Du kannst einen Teil oder deine gesamte Bewegung nutzen, um dich zwischen diesen Angriffen zu bewegen.",
+            "Falls du bei dieser Aktion mit einer leichten Waffe angreifst, kannst du später im selben Zug einen zusätzlichen Angriff als Bonusaktion mit einer anderen leichten Waffe ausführen.",
+            "Immer wenn du bei dieser Aktion den unbewaffneten Schlag einsetzt, kannst du wählen, ob du mit dem Zeil <i>ringst</i> oder das Ziel <i>stößt</i>.",
+            "Einige Zustände verschaffen dir einen Vorteil beim Angriff: Angriffe gegen Ziele mit dem Zustand „geblendet“, „gelähmt“, „versteinert“, „gefesselt“, „betäubt“ oder „bewusstlos“; Angriffe gegen Ziele mit dem Zustand „liegend“ im Umkreis von 5 Fuß um dich; Angriffe, während du den Zustand „unsichtbar“ hast.",
+            "Einige Zustände führen zu einem Nachteil beim Angriff: Angriffe gegen Ziele mit dem Zustand „Unsichtbar“; Angriffe gegen Ziele mit dem Zustand „Liegend“ aus einer Entfernung von mehr als 5 Fuß; Angriffe, während du den Zustand „Geblendet“, „Verängstigt“, „Vergiftet“, „Liegend“ oder „Gefesselt“ hast." 
         ]
     },
     {
@@ -33,8 +34,7 @@ data_action = [
             "For the rest of the turn, give yourself extra movement equal to your <i>Speed</i>."
         ],
         bullets_de: [
-            "Muss aktualisiert werden:",
-            "Die Erhöhung entspricht deiner Geschwindigkeit, nachdem alle Bonus angerechnet wurden."
+            "Für den Rest der Runde erhältst du eine zusätzliche Bewegung in Höhe deiner „Geschwindigkeit“."
         ]
     },
     {
@@ -62,42 +62,7 @@ data_action = [
         ],
         bullets_de: [
             "Bis zum Start deines nächsten Zuges ist jeder Angriff gegen dich im Nachteil, wenn du den Angreifer sehen kannst. Außerdem machst du Geschicklichkeits-Rettungswürfe mit Vorteil.",
-			"Du verlierst diese Vorteile, wenn du <i>kampfunfähig</i> wirst oder wenn deine Geschwindigkeit auf 0 fällt."
-        ]
-    },
-    {
-        title: "TODO: Cast a spell",
-        icon: "magic-swirl",
-        subtitle: "Zauberwirken",
-        description: "Cast a spell with a casting time of 1 action | Wirke einen Zauberspruch mit einer Zauberzeit von 1 Aktion",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "The target of a spell must be within the spell's range. To target something, you must have a clear path to it, so it can't be behind total cover.",
-            "Spells with material components do not consume the material unless explicitly stated. Unless the cost of a material is given, you can assume that the cost is negligible and the material is simply available in a component pouch.",
-            "Some spells require you to maintain concentration in order to keep their magic active. If you lose concentration, such a spell ends. You lose concentration on a spell if you cast another spell that requires concentration or when you are incapacitated. Each time you take damage, you must make a Constitution saving throw to maintain your concentration. The DC equals 10 or half the damage you take, whichever number is higher."
-        ],
-        bullets_de: [
-            "Du kannst mit deiner Aktion keinen Zauberspruch wirken, während du gleichzeitig einen anderen Zauberspruch mit deiner Bonusaktion wirkst, es sei denn, die Aktion wird für einen Zaubertrick verwendet.",
-			"Das Ziel eines Zauberspruchs muss innerhalb der Reichweite des Zauberspruchs sein. Um zu zielen muss der Pfad zwischen dir und dem Ziel frei sein, d.h. das Ziel darf nicht hinter voller Deckung sein.",
-			"Zaubersprüche mit Material-Komponenten verbrauchen das Material nicht, sofern dies nicht explizit angegeben ist. Wenn keine Kosten für das Material angegeben sind, kannst du annehmen, dass die Kosten vernachlässigbar sind und dass das Material in deiner Komponententasche verfügbar ist.",
-            "Manche Zaubersprüche erfordern, dass du Konzentration aufrecht erhältst um ihre Magie aktiv zu halten. Wenn du die Konzentration verlierst, endet ein solcher Zauberspruch. Du verlierst die Konzentration, wenn du einen anderen Spruch wirkst, der ebenfalls Konzentration erfordert, oder wenn du kampfunfähig bist. Jedes Mal, wenn du Schaden nimmst, musst du eine Probe auf Konstitution werfen. Der Herausforderungsgrad entspricht 10 oder der Hälfte des Schadens, den du nimmst, je nachdem was höher ist.",
-			"Zaubersprüche mit einer Zauberzeit von mehr als 1 Aktion müssen über mehrere Runden gezaubert werden, bis die Zauberzeit erreicht ist. Dazu wendest du in jeder Runde deine Aktion auf. Während dieser Zeit musst du Konzentration aufrecht erhalten. Schaffst du es nicht die Konzentration zu halten, bricht der Zauber ab, aber es wird kein Zauberplatz verbraucht."
-        ]
-    },
-    {
-        title: "TODO: Escape",
-        icon: "manacles",
-        subtitle: "Entkommen",
-        description: "Escape a grapple | Entkomme einem Haltegriff",
-        reference: "",
-        bullets: [
-            "To escape a grapple, you must succeed on a Strength (Athletics) or Dexterity (Acrobatics) check contested by the grappler's Strength (Athletics) check.",
-            "Escaping other conditions that restrain you (such as manacles) may require a Dexterity or Strength check, as specified by the condition."
-        ],
-        bullets_de: [
-            "Um einem Haltegriff zu entkommen, musst du eine Probe auf Stärke (Athletik) oder Geschicklichkeit (Akrobatik) gegen eine Probe auf Stärke (Athletik) der Kreatur, die dich gepackt hält, gewinnen.",
-			"Anderen Umständen, die dich festhalten (zum Beispiel Handschellen) zu entkommen, kann eine Probe auf Geschicklichkeit oder Stärke erfordern, je nach Anforderung des Umstands."
+			"Du verlierst diese Vorteile, wenn du „kampfunfähig“ wirst oder wenn deine Geschwindigkeit auf 0 fällt."
         ]
     },
     {
@@ -108,42 +73,13 @@ data_action = [
         reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#HelpAction'>Help in Free Rules</a>",
         bullets: [
             "An ally gains advantage on the next ability check they make with the skill or tool you assist with.",
-            " You momentarily distract an enemy within 5 feet of you, giving Advantage to the next attack roll by one of your allies against that enemy. This benefit expires at the start of your next turn.",
+            "You momentarily distract an enemy within 5 feet of you, giving Advantage to the next attack roll by one of your allies against that enemy. This benefit expires at the start of your next turn.",
             "You can try to stabilize a creature with 0 Hit Points or remove the <i>Unconscious</i> condition of a create with 1 or more Hit Points, which requires a successful DC 10 Wisdom (Medicine) check."
         ],
         bullets_de: [
-            "Muss aktualisiert werden:",
-            "Das Ziel erhält Vorteil auf die nächste Fähigkeitenprobe, die es macht um die Aufgabe zu erfüllen, bei der du ihr hilfst.",
-			"Alternativ erhält das Ziel Vorteil auf den nächsten Angriff gegen ein bestimmtes Ziel innerhalb von 5 Fuß neben dir.",
-            "Der Vorteil ist einmalig bis zum Beginn deines nächsten Zuges nutzbar."
-        ]
-    },
-    {
-        title: "Utilize",
-        icon: "snatch",
-        subtitle: "Gegenstand verwenden",
-        description: "Use a nonmagical object. | Einen nichtmagischen Gegenstand verwenden.",
-        reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#UtilizeAction'>Utilize in Free Rules</a>",
-        bullets: [
-            "You normally interact with an object while doing something else, such as when you draw a sword as part of the Attack action. When an object requires an action for its use, you take the Utilize action."
-        ],
-        bullets_de: [
-            "Normalerweise interagierst du mit einem Gegenstand, während du etwas anderes tust, z.B. wenn du ein Schwert als Teil der Aktion Angriff ziehst. Wenn ein Gegenstand eine Aktion erfordert, um ihn zu benutzen, führst du diese Aktion aus."
-        ]
-    },
-    {
-        title: "TODO: Use shield",
-        icon: "round-shield",
-        subtitle: "Schild nutzen",
-        description: "Equip or unequip a shield | Lege einen Schild an oder ab",
-        reference: "PHB, pgs. 144-146.",
-        bullets: [
-            "A shield always takes an action to equip or unequip.",
-            "Armor takes several minutes to equip or unequip."
-        ],
-        bullets_de: [
-            "Einen Schild an- oder abzulegen erfordert eine Aktion.",
-			"Rüstung an- oder abzulegen dauert mehrere Minuten (1 min Leichte Rüstung, 5 min Mittelschwere Rüstung, 10 min Schwere Rüstung)",
+            "Ein Verbündeter erhält einen Vorteil bei der nächsten Fähigkeits-Check, die mit der Fertigkeit oder dem Werkzeug ausgeführt wird, bei der du hilfst.",
+            "Du lenkst einen Feind im Umkreis von 5 Fuß von dir kurzzeitig ab und dadurch erhält einer deiner Verbündeten einen Vorteil auf den nächsten Angriffswurf gegen diesen Feind. Dieser Vorteil verfällt zu Beginn deines nächsten Zuges.",
+			"Du kannst versuchen, eine Kreatur mit 0 Trefferpunkten zu stabilisieren oder den Zustand „Bewusstlosigkeit“ einer Kreatur mit 1 oder mehr Trefferpunkten zu beseitigen, was einen erfolgreichen DC 10 Weisheit (Medizin) Check erfordert."
         ]
     },
     {
@@ -158,13 +94,41 @@ data_action = [
             "The condition ends on you immediately after any of the following occurs: you make a sound louder than a whisper, an enemy finds you, you make an attack roll, or you cast a spell with a Verbal component."
         ],
         bullets_de: [
-            "Muss aktualisiert werden:",
-            "Du kannst dich nicht vor einer Kreatur verstecken, die dich sehen kann. Du musst volle Deckung haben, in einem komplett verschleierten Bereich sein, unsichtbar sein oder anderweitig die Sicht des Feindes blockieren.",
-            "Wirf eine Probe auf Geschicklichkeit (Heimlichkeit), wenn du versuchst dich zu verstecken und notiere das Ergebnis. Bis du entdeckt wirst, oder aufhörst dich zu verstecken wird dieses Ergebnis von Proben auf Weisheit (Wahrnehmung) von Kreaturen, die aktiv nach dir oder nach Zeichen deiner Anwesenheit suchen, gekontert.",
-            "Eine Kreatur bemerkt dich auch ohne zu suchen, wenn das Ergebnis deiner Heimlichkeits-Probe kleiner oder gleich seiner passiven Wahrnehmung ist.",
-			"Wenn du Geräusche machst (zum Beispiel eine Warnung rufen, oder eine Vase umstoßen), verrätst du deine Position und wirst entdeckt.",
-			"Ob du aus deinem Versteck heraustreten und trotzdem noch für den Rest deines Zuges als versteckt gelten kannst, entscheidet der DM.",
-            "Außerhalb des Kampfes kannst du außerdem Proben auf Geschicklichkeit (Heimlichkeit) ablegen um dich vor Feinden zu tarnen, an Wachen vorbei zu schleichen, entkommen ohne bemerkt zu werden oder an jemanden heran zu schleichen ohne gesehen oder gehört zu werden.",
+            "Muss aktualisiert werden!"
+        ]
+    },
+    {
+        title: "Influence",
+        icon: "charm",
+        subtitle: "Beeinflussen",
+        description: "You urge a monster to do something. | Du drängst ein Monster dazu, etwas zu tun.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#InfluenceAction'>Influence in Free Rules</a>",
+        bullets: [
+            "Make a Charisma (Deception, Intimidation, Performance, or Persuasion) or Wisdom (Animal Handling) check to alter a creature’s attitude.",
+            "Make a <i>Deception</i> check to deceive a monster that understands you.",
+            "Make a <i>Intimidation</i> check to intimidate a monster.",
+            "Make a <i>Performance</i> check to amuse a monster.",
+            "Make a <i>Persuasion</i> check to persuade a monster that understands you.",
+            "Make an <i>Animal Handling</i> check to gently coax a Beast or Monstrosity."
+        ],
+        bullets_de: [
+            "Muss aktualisiert werden!"
+        ]
+    },
+    {
+        title: "Magic",
+        icon: "magic-swirl",
+        subtitle: "Zauberwirken",
+        description: "Cast a spell, use a magic item, or use a magical feature. | Wirke einen Zauberspruch, benutze einen magischen Gegenstand oder eine magische Eigenschaft.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#MagicAction'>Magic in Free Rules</a>",
+        bullets: [
+            "You cast a spell that has a casting time of an action or use a feature or magic item that requires a Magic action to be activated.",
+            "If you cast a spell that has a casting time of 1 minute or longer, you must take the Magic action on each turn of that casting, and you must maintain Concentration while you do so. If your Concentration is broken, the spell fails, but you don’t expend a spell slot.",
+            "On a turn, you can expend only one spell slot to cast a spell. This rule means you can’t, for example, cast a spell with a spell slot using the Magic action and another one using a Bonus Action on the same turn.",
+            "Some spells and other effects require Concentration to remain active, as specified in their descriptions. If the effect’s creator loses Concentration, the effect ends. You loose Concentration the moment you start casting a spell or activate another effect that requires Concentration or if you take damage and fail a DC 10 or half the damage taken (whichever ist higher) or you have the <i>Incapacitated</i> condition or you die."
+        ],
+        bullets_de: [
+            "Muss aktualisiert werden!"
         ]
     },
     {
@@ -200,21 +164,40 @@ data_action = [
             "Make a <i>Survival</i> check to detect tracks or food."
         ],
         bullets_de: [
-            "Muss aktualisiert werden:",
-            "Abhängig davon wonach und wie du suchst, kann der DM eine Probe auf Weisheit (Wahrnehmung) oder auf Intelligenz (Nachforschen) verlangen."
+            "Muss aktualisiert werden!"
         ]
     },
     {
-        title: "TODO: Use class feature",
-        icon: "embrassed-energy",
-        subtitle: "Nutze ein Merkmal",
-        description: "",
-        reference: "See class page for more information. | Siehe die Beschreibung des Merkmals",
+        title: "Study",
+        icon: "book-aura",
+        subtitle: "Untersuchen",
+        description: "Study your memory, a book, a clue, or another source of knowledge | Rufe dir etwas in Erinnerung oder untersuche ein Buch, einen Hinweis oder eine anderen Wissensquelle.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#StudyAction'>Study in Free Rules</a>",
         bullets: [
-            "Use a racial or class feature that uses an action."
+            "You make an Intelligence check to study your memory, a book, a clue, or another source of knowledge and call to mind an important piece of information about it.",
+            "Make an <i>Arcana</i> check to study spells, magic items, eldritch symbols, magical traditions, planes of existence, and certain creatures (Aberrations, Constructs, Elementals, Fey, and Monstrosities).",
+            "Make a <i>History</i> check to study historic events and people, ancient civilizations, wars, and certain creatures (Giants and Humanoids).",
+            "Make an <i>Investigation</i> check to study traps, ciphers, riddles, and gadgetry.",
+            "Make a <i>Nature</i> check to studay terrain, flora, weather, and certain creatures (Beasts, Dragons, Oozes, and Plants).",
+            "Make a <i>Religion</i> check to study deities, religious hierarchies and rites, holy symbols, cults, and certain creatures (Celestials, Fiends, and Undead)."
         ],
         bullets_de: [
-            "Nutze ein Rassen- oder Klassenmerkmal, das eine Aktion verbraucht."
+            "Muss aktualisiert werden!"
+        ]
+    },
+    {
+        title: "Utilize",
+        icon: "snatch",
+        subtitle: "Gegenstand verwenden",
+        description: "Use a nonmagical object. | Einen nichtmagischen Gegenstand verwenden.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/free-rules/rules-glossary#UtilizeAction'>Utilize in Free Rules</a>",
+        bullets: [
+            "You normally interact with an object while doing something else, such as when you draw a sword as part of the Attack action. When an object requires an action for its use, you take the Utilize action.",
+            "Shields require the Utilize action to Don or Doff."
+        ],
+        bullets_de: [
+            "Normalerweise interagierst du mit einem Gegenstand, während du etwas anderes tust, z.B. wenn du ein Schwert als Teil der Aktion Angriff ziehst. Wenn ein Gegenstand eine Aktion erfordert, um ihn zu benutzen, führst du diese Aktion aus.",
+            "Um ein Schild anzulegen oder abzulegen ist diese Aktion erforderlich."
         ]
     },
     {
